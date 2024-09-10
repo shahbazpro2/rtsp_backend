a132711748e8146cb75595317aa64e801674364d
Muhammad <shahbaz.pro2@gmail.com>
2024-09-11 02:21:39 +0500

    Changes

--- index.js
+++ index.js
@@ -10,12 +10,19 @@ const app = express();
 app.use(cors());
 const server = http.createServer(app);
 const io = require("socket.io")(server, { cors: { origin: "*" } });
+let currentQueueData = {
+  [queues[0]]: {},
+  [queues[1]]: {},
+};
+let stream, singleStream;
 
-const stream = new Stream({
-  name: "Bunny",
-  streamUrl: "rtsp://admin:zxcvbnm0.@190.92.4.249:554/cam/realmonitor?channel=1&subtype=0",
-  wsPort: 6789,
-  /*  ffmpegOptions: {
+app.get("/stream", (req, res) => {
+  const stream1 = () => {
+    stream = new Stream({
+      name: "Bunny",
+      streamUrl: "rtsp://127.0.0.1:8554/ds-test",
+      wsPort: 6789,
+      /*  ffmpegOptions: {
       "-f": "mpegts",
       "-codec:v": "mpeg1video",
       "-b:v": "1000k",
@@ -28,20 +35,61 @@ const stream = new Stream({
       "-ac": 1,
       "-b:a": "128k",
     }, */
+    });
+  };
+  if (stream) {
+    stream.stop();
+  }
+  stream1();
+  res.send({ message: "success" });
+});
+
+app.get("/stream/:id", (req, res) => {
+  const parms = req.params.id;
+  const cameraData = currentQueueData[queues[0]]?.[parms];
+  if (!cameraData) {
+    res.send({ message: "Camera not found" });
+  }
+  const stream1 = () => {
+    singleStream = new Stream({
+      name: "Single Camera",
+      streamUrl: cameraData,
+      wsPort: 6790,
+      /*  ffmpegOptions: {
+          "-f": "mpegts",
+          "-codec:v": "mpeg1video",
+          "-b:v": "1000k",
+          "-stats": "",
+          "-r": 25,
+          "-s": "1920x1080",
+          "-bf": 0,
+          "-codec:a": "mp2",
+          "-ar": 44100,
+          "-ac": 1,
+          "-b:a": "128k",
+        }, */
+    });
+  };
+  if (stream) {
+    singleStream.stop();
+  }
+  stream1();
+  res.send({ message: "success" });
 });
 
 const port = 8000;
 
-let currentQueueData = {
-  [queues[0]]: {},
-  [queues[1]]: {},
-};
 // Serve static files from the 'public' directory
 app.use(express.static("public"));
 
 // Handle a Socket.IO connection
 io.on("connection", (socket) => {
-  console.log("New client connected");
+  console.log("New client connected", currentQueueData[queues[0]]);
+
+  socket.on("initialData", () => {
+    io.emit(queues[0], currentQueueData[queues[0]]);
+    io.emit(queues[1], currentQueueData[queues[1]]);
+  });
 
   Rabbitmq(currentQueueData, io);
 
